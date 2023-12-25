57838f6d83c82de013f2c4a1dffe73587738c07a
ATA <ata.attarian78@gmail.com>
2023-12-25 21:12:33 +0330

    change static settings

--- levelCampBackend/settings.py
+++ levelCampBackend/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
-STATIC_URL = 'statics/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
