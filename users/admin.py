b49164e20fb62f1c1c52b5f0dd3c321bede095fb
ATA <ata.attarian78@gmail.com>
2024-01-07 19:53:50 +0330

    add phonenumber to admin list display

--- users/admin.py
+++ users/admin.py
@@ -15,7 +15,7 @@ class CustomUserChangeForm(forms.ModelForm):
 
 class UserAdmin(BaseUserAdmin):
     form = CustomUserChangeForm
-    list_display = ['email', 'first_name', 'last_name', 'is_active', 'is_staff']
+    list_display = ['email', 'first_name', 'last_name','phoneNumber', 'is_active', 'is_staff']
     search_fields = ['email', 'first_name', 'last_name']
     list_filter = ['is_active', 'is_staff']
     fieldsets = (
